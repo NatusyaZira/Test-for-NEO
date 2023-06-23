3a5db9918bb381b9358128e37b6d287233c31848
Nataliia Kovalova <natusya.zira@gmail.com>
2023-06-23 20:37:43 +0300

    Created vite-app, created one card

---
+++ NEO-App/src/components/Card.jsx
@@ -0,0 +1,65 @@
+import {  Box,
+    Card,
+    CardBody,
+    Heading,
+    CardHeader,
+    Stack,
+    StackDivider,
+    Text } from '@chakra-ui/react';
+
+
+const CreateCard = ({ data})=> {
+    const {
+        maxEstimatedDiameter,
+        numberOfPotentiallyHazardousNEOs,
+        fastestNEO,
+        closestNEO,
+        date
+    } = data;
+    
+    return (
+        <Card >
+            <CardHeader> <Heading className='heading' size='xs'>{date}</Heading>
+            </CardHeader>
+            <CardBody className='card'>
+            <Stack divider={<StackDivider />} spacing='4'>
+                <Box>
+                    <Heading as='h3' size='xs'>
+                       {maxEstimatedDiameter} km
+                    </Heading>
+                     <Text>
+                    max estimated diameter of NEO in kilometers for the day
+                    </Text>
+                </Box>
+                <Box>
+                    <Heading as='h3' size='xs'>
+                    {numberOfPotentiallyHazardousNEOs} pcs
+                    </Heading>
+                    <Text>
+                    number of potentially hazardous NEOs per day
+                    </Text>
+                </Box>
+                 <Box>
+                    <Heading as='h3' size='xs'>
+                     {closestNEO} km
+                    </Heading>
+                    <Text>
+                    closest NEO
+                    </Text>
+                </Box>
+                <Box>
+                     <Heading as='h3' size='xs'>
+                     {fastestNEO} kph
+                    </Heading>
+                    <Text>
+                    fastest NEO
+                    </Text>
+                </Box>
+                </Stack>
+                </CardBody>
+                </Card>
+
+    )
+}
+
+export default CreateCard;
